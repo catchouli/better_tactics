e517485db62c908b0f8bd50f029df132791b7997
Caitlin Wilks <cat@cat.bio>
2023-09-26 17:25:25 +0100

    Fixed the tactics board

--- assets/better-tactics.js
+++ assets/better-tactics.js
@@ -5,67 +5,90 @@ import { Chessground } from './deps/chessground.min.js';
 
 export class Puzzle {
     constructor(container, puzzle_id, fen, moves, rating) {
-        this.puzzle_id = puzzle_id;
-        this.fen = fen;
-        this.moves = moves.split(" ");
-        this.rating = rating;
+        this._puzzle_id = puzzle_id;
+        this._fen = fen;
+        this._moves = moves.split(" ");
+        this._rating = rating;
 
-        this.game = new Chess(fen);
+        this._game = new Chess(fen);
+        this._board = Chessground(container);
 
         // Figure out who should move first (the move that sets up the tactic), make the move,
         // and let the player make the next move.
-        let first_move_orig = this.moves[0].slice(0, 2);
-        let first_move_color = this.game.get(first_move_orig).color == 'w' ? 'white' : 'black';
-        let player_color = first_move_color == 'white' ? 'black' : 'white';
+        let first_move_origin_square = this._moves[0].slice(0, 2);
 
-        console.log("first move color: " + first_move_color + ", player color: " + player_color);
+        this._computer_color = this._game.get(first_move_origin_square).color;
+        this._player_color = this._computer_color == 'w' ? 'b' : 'w';
 
-        this.chessground = Chessground(container, {
-            movable: {
-                free: true,
-                color: player_color,
-                showDests: true,
-                events: {
-                    after: this.after_move.bind(this)
-                },
-            },
-            game: this.game,
-            fen: this.fen
-        });
-
-        this.make_next_move();
+        // Start puzzle.
+        this.reset();
     }
 
-    make_next_move() {
-        console.log("making next move");
-        console.log(this.game._turn);
-        let next_move = this.moves.shift();
+    reset() {
+        this._game = new Chess(this._fen);
+
+        this._remaining_moves = this._moves.slice();
+
+        this._board.set({
+            movable: {
+                // TODO: only accept legal moves.
+                free: true,
+                showDests: true,
+                events: {
+                    after: this._after_move.bind(this)
+                },
+            },
+            orientation: this._player_color == 'w' ? 'white' : 'black',
+            turnColor: this._computer_color == 'w' ? 'white' : 'black',
+            fen: this._fen
+        });
+
+        this._make_next_move();
+    }
+
+    _make_next_move() {
+        if (this._game.turn() != this._computer_color) {
+            console.warn("_make_next_move(): called when it's the player's turn");
+            return;
+        }
+
+        console.log("Making next move");
+
+        let next_move = this._remaining_moves.shift();
         if (next_move) {
             let source = next_move.slice(0, 2);
             let dest = next_move.slice(2);
 
-            console.log("Making computer move:" + next_move + " (" + source  + " to " + dest + ")");
-            this.game.move(next_move);
-            this.chessground.move(source, dest);
-            console.log(this.game._turn);
+            this._game.move(next_move);
+            this._board.set({
+                fen: this._game.fen(),
+                lastMove: [source, dest],
+                turnColor: this._player_color == 'w' ? 'white' : 'black'
+            });
+            this._board.move(next_move);
         }
     }
 
-    after_move(orig, dest, metadata) {
-        console.log("after move");
-        console.log(this.game._turn);
-        if (this.moves.length > 0) {
+    _after_move(orig, dest, metadata) {
+        if (this._remaining_moves.length > 0) {
+            // Make the move in chess.js.
             let move = orig + dest;
-            this.game.move(move);
-            console.log(this.game._turn);
+            this._game.move(move);
 
-            if (this.moves[0] == move) {
-                console.log('right move!');
-                this.moves.shift();
-                this.make_next_move();
+            if (this._remaining_moves[0] == move) {
+                console.log('Right move!');
+                this._remaining_moves.shift();
+
+                // If there are no moves left, the puzzle is complete.
+                if (this._remaining_moves.length == 0) {
+                    console.log("Puzzle complete!");
+                }
+                else {
+                    this._make_next_move();
+                }
             }
             else {
-                console.log('wrong :(');
+                console.log('Wrong move :(');
             }
         }
     }
